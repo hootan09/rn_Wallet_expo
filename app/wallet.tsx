1881d6e0ae48eb82d0c392a7c14a5264a96b236d
hootan09 <hootan09@gmail.com>
2025-10-12 14:18:30 +0330

    working on wallet

--- app/wallet.tsx
+++ app/wallet.tsx
@@ -1,7 +1,10 @@
-import { Colors } from '@/constants/theme'
+import { Colors, Fonts } from '@/constants/theme'
+import AntDesign from '@expo/vector-icons/AntDesign'
+import Feather from '@expo/vector-icons/Feather'
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
 import { useRouter } from 'expo-router'
 import React, { useState } from 'react'
-import { Button, StyleSheet, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Wallet = () => {
@@ -14,27 +17,47 @@ const Wallet = () => {
         desc: "We're fetching your wallet details..."
     })
 
-    const onVerifyRecoveryPress = () => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-            router.navigate('/wallet');
-        }, 15000);
-        setTimeout(() => {
-            setNotifyTexts({
-                title: 'Almost there!',
-                desc: 'Syncing with the blockchain...'
-            })
-        }, 7000);
-    }
+
 
     return (
         <SafeAreaView style={styles.container}>
-            {/* <TouchableOpacity style={styles.backBtn} onPress={() => router.back()}>
-                <FontAwesome5 name="long-arrow-alt-left" size={22} color={Colors.light.white} />
-            </TouchableOpacity> */}
 
-            <View style={{justifyContent: 'center', alignItems: 'center', flex:1}}>
+            <View style={styles.topWrapper}>
+                <View style={styles.topLeftBtn}>
+                    <TouchableOpacity>
+                        <AntDesign name="setting" size={24} color={Colors.light.white} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <AntDesign name="scan" size={24} color={Colors.light.white} />
+                    </TouchableOpacity>
+                </View>
+
+                <TouchableOpacity style={styles.topCenterBtn}>
+                    <Text style={styles.mainWalletTxt}>Main Wallet</Text>
+                    <AntDesign name="caret-down" size={20} color={Colors.light.white} />
+                </TouchableOpacity>
+
+                <View style={styles.topRightBtn}>
+                    <TouchableOpacity>
+                        <FontAwesome5 name="copy" size={24} color={Colors.light.white} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity>
+                        <FontAwesome5 name="search" size={24} color={Colors.light.white} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.balanceWrapper}>
+                <Text style={styles.balanceText}>$678.17</Text>
+                <View style={styles.balanceProfitWrapper}>
+                    <Feather name="arrow-up" size={25} color={Colors.light.accent} />
+                    <Text style={styles.balanceProfitText}>$15.49 (-1.97%)</Text>
+                </View>
+            </View>
+
+            <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
                 <Button title='Start Again!' onPress={() => router.navigate('/')} color={Colors.light.primary} />
             </View>
         </SafeAreaView>
@@ -52,5 +75,59 @@ const styles = StyleSheet.create({
     backBtn: {
         marginTop: 20,
         marginLeft: 16,
-    }
+    },
+    topWrapper: {
+        top: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "space-between",
+    },
+    topLeftBtn: {
+        flexDirection: "row",
+        gap: 25,
+    },
+    topCenterBtn: {
+        flexDirection: "row",
+        gap: 5,
+        alignItems: 'center',
+    },
+    mainWalletTxt: {
+        fontFamily: Fonts.rounded,
+        fontSize: 15,
+        lineHeight: 18,
+        fontWeight: '700',
+        color: Colors.light.white
+    },
+    topRightBtn: {
+        flexDirection: "row",
+        gap: 25,
+    },
+    balanceWrapper: {
+        top: 90,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: 5,
+    },
+    balanceText: {
+        fontFamily: Fonts.rounded,
+        fontSize: 45,
+        lineHeight: 50,
+        fontWeight: '700',
+        color: Colors.light.white,
+    },
+    balanceProfitWrapper: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 4,
+    },
+    balanceProfitText: {
+        fontFamily: Fonts.rounded,
+        fontSize: 20,
+        lineHeight: 24,
+        fontWeight: '500',
+        color: Colors.light.accent,
+    },
 })
